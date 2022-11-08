7155e35b92934f151408a5396d29ebe5a3369763
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2022-11-09 01:26:38 +0530

    added grass and textures for city

--- src/controllers/loadCar.js
+++ src/controllers/loadCar.js
@@ -10,24 +10,22 @@ export default function loadCar(callback) {
     "./assets/yellowCar.fbx",
     function (object) {
       object.traverse(function (child) {
-        let receiveShadow = true;
+        // let receiveShadow = true;
         console.log(child, child.name.includes("wheel"));
         if (child.isMesh) {
           if (child.name == "carBody") {
             game.car.carBody = child;
 
             child.castShadow = true;
-            receiveShadow = false;
           } else if (child.name.includes("wheel")) {
             game.car.wheel = child;
 
             // child.parent = game.scene;
             child.visible = true;
             child.castShadow = true;
-            receiveShadow = false;
           }
 
-          child.receiveShadow = receiveShadow;
+          child.receiveShadow = true;
         }
       });
 
